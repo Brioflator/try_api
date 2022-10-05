05faad06c6078646ef8702ef7c187ecc577dccb3
Brioflator <75537253+Brioflator@users.noreply.github.com>
2022-10-06 00:24:34 +0300

    Create example4.cpp

---
+++ example4.cpp
@@ -0,0 +1,15 @@
+#include<vector>
+#include<algorithm>
+typedef struct Block {
+    int value;
+    Block(int value): value(value) {}
+} Block;
+
+int main() {
+    std::vector<Block> blocks;
+    blocks.emplace_back(1);
+    Block& block = blocks[0];
+    blocks.emplace_back(2);
+    std::max(1, block.value + 1);
+    return 0;
+}
